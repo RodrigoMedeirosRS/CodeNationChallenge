226f26487247d011c730d1fef2de03ff84732571
Rodrigo Medeiros Lehnemann <rodrigo.medeiros.email@gmail.com>
2019-03-25 09:25:13 -0300

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -1,5 +1,8 @@
 #Code made by R.M.Lehnemann into 24/03/2018, published into 25/03/2018
 
+#User License:
+# All code are distributed under LGPL by Rodrigo M.Lehnemann
+
 class myResult:
     first = True
     nationalities = []
